2d9faa92a948058404ab3ca733d5fba693eae37c
mariaaortega <antuortega@gmail.com>
2022-01-17 11:46:22 -0300

    primer commit

---
+++ Models/Team.cs
@@ -0,0 +1,7 @@
+namespace Clase3Mvc.Models;
+public class Team {
+
+public int Id {get; set;}
+public string Name {get; set;}
+
+}
---
+++ Models/TeamContext.cs
@@ -0,0 +1,11 @@
+using Clase3Mvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class TeamContext : DbContext
+{
+    public TeamContext(DbContextOptions<TeamContext > options): base (options)
+    {
+    }
+    public DbSet<Team> Teams { get ; set; }
+
+}
